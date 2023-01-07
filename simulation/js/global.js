b9857480b51602ee178e2769725a024fabce66f8
vleadadmin <admin@vlabs.ac.in>
2023-01-07 10:57:32 +0000

    https://virtual-labs.github.io/virtual-labs/exp-nfa-to-dfa-iiith click on the link to test your code.

---
+++ simulation/js/global.js
@@ -0,0 +1,30 @@
+/****
+  * File containing global variables
+  *
+  */
+
+width = 500;
+height = 200;
+radius = 25;
+
+// nfa = [nfa1, nfa2, nfa3];
+nfa = [nfa1, nfa2];
+nfaIndex = 0
+
+inputIndex = 0
+inputPointer = -1
+
+nfa_nodes = []
+nfa_edges = []
+
+path_state = "acc";
+
+// dfa = [dfa1, dfa2, dfa3];
+dfa = [dfa1, dfa2];
+dfaIndex = 0
+
+inputIndex = 0
+inputPointer = -1
+
+dfa_nodes = []
+dfa_edges = []
